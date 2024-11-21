64ac4f8211d6ba2b6b509410213befc17d443ec9
taylor <taylor@bluestem.solutions>
2024-11-20 21:45:19 -0500

    finished part 6, generic list and detail views

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -46,6 +46,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a particular book instance."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
 
