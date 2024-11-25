9e068d9868439de0860dcf23da475d9a1ccebfca
taylor learns machines <taylor@bluestem.solutions>
2024-11-25 15:13:29 -0500

    configured allowed hosts

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$%31$x4&mohlo6
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -154,3 +154,12 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+ALLOWED_HOSTS = ['mdn-django-production.up.railway.app', '127.0.0.1']
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://mdn=django=production.up.railway.app']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
